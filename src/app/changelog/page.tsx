661297abd092368d2d043eb5247d6091d14c0b56
Simon <wsj_simon@163.com>
2025-06-17 22:25:25 +0800

    feat: 添加 CHANGELOG 页面

--- src/app/changelog/page.tsx
+++ src/app/changelog/page.tsx
@@ -1,7 +1,20 @@
 import React from 'react';
+import ReactMarkdown from 'react-markdown';
+import { readFile } from 'fs/promises';
+import { join } from 'path';
 
-const ChangeLogPage = () => {
-  return <div>Change Log</div>;
+const ChangeLogPage = async () => {
+  // 在服务器端读取 CHANGELOG.md 文件
+  const changelogPath = join(process.cwd(), 'CHANGELOG.md');
+  const changelogContent = await readFile(changelogPath, 'utf8');
+
+  return (
+    <div className="container mx-auto max-w-4xl px-4 py-8">
+      <div className="prose prose-lg prose-gray max-w-none dark:prose-invert">
+        <ReactMarkdown>{changelogContent}</ReactMarkdown>
+      </div>
+    </div>
+  );
 };
 
 export default ChangeLogPage;
